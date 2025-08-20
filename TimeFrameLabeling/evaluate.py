74fed4e2e5510171b001705b8fca6a3e15edc467
shel-ho <business.sph@gmail.com>
2025-08-20 12:58:19 -0400

    Evaluate LID

--- TimeFrameLabeling/evaluate.py
+++ TimeFrameLabeling/evaluate.py
@@ -18,8 +18,6 @@ NOTICE: Currently only evaluates chyron and slate detection
 from common import ClamsAAPBEvaluationTask
 
 # Constants
-## Note that this repository is private and the files are not available to the public (due to IP concerns), 
-## so using goldretriever to download the gold files WILL NOT work (goldretriever is only for public repositories)
 GOLD_CHYRON_URL = "https://github.com/clamsproject/aapb-annotations/tree/106-chyronunderstanding/newshour-chyron/golds"
 GOLD_SLATES_URL = "https://github.com/clamsproject/aapb-annotations/tree/106-chyronunderstanding/january-slates/golds"
 
@@ -32,12 +30,19 @@ class TimeFrameLabelingEvaluator(ClamsAAPBEvaluationTask):
     LID uses Identification Error Rate and Identification Precision, Recall, and F1.
 
     Note that for error rate, lower is better and can be greater than 100%.
-    We use `pyannote` python library as the implementation for all metrics. .
-    More details can be found https://pyannote.github.io/pyannote-metrics/reference.html
+    For all metrics, we use the `pyannote` library. 
+    More details can be found at https://pyannote.github.io/pyannote-metrics/reference.html
     """
+
     def __init__(self, batchname: str, **kwargs):
         super().__init__(batchname, **kwargs)
-        self.slates = kwargs.get('slates')
+        self.task = kwargs.get('task')
+        
+        task_dict = {'chyron': 'Chyron Detection', 
+                     'slate': 'Slate Detection',
+                     'lid': 'Language Identification',
+                    }
+        self._taskname = task_dict[self.task]
 
     def _read_gold(self, gold_file: Union[str, Path], **kwargs) -> Any:
         """
@@ -46,12 +51,16 @@ class TimeFrameLabelingEvaluator(ClamsAAPBEvaluationTask):
         """
         gold_annotation = Annotation()
         col_name = 'scene-label'
-        if self.slates:
+        if self.task == 'slate':
             col_name = 'scene-type'
         for _, row in pd.read_csv(gold_file).iterrows():
             if row['start'] != 'NO':
+                label = row[col_name]
+                #Currently sets 'ceb' to 'lr' because cebuano cannot be predicted by the whisper model
+                if self.task == 'lid' and (label != 'en' or label != 'es'):
+                    label = 'lr'
                 gold_annotation[Segment(tuh.convert(row['start'], 'iso', 'seconds', 1),
-                                        tuh.convert(row['end'], 'iso', 'seconds', 1))] = row[col_name]
+                                        tuh.convert(row['end'], 'iso', 'seconds', 1))] = label
         return gold_annotation
 
     def _read_pred(self, pred_file: Union[str, Path], gold: Optional[Any], **kwargs) -> Tuple[Any, Optional[Any]]:
@@ -62,6 +71,11 @@ class TimeFrameLabelingEvaluator(ClamsAAPBEvaluationTask):
         data = Mmif(mmif_str)
         fps = 29.97
 
+        if self.task == 'lid':
+            property_name = 'label'
+        else:
+            property_name = 'frameType'
+
         tf_view = data.get_view_contains(AnnotationTypes.TimeFrame)
         for ann in reversed(list(tf_view.get_annotations(AnnotationTypes.TimeFrame))):
             if ann.get_property('fps'):
@@ -70,29 +84,42 @@ class TimeFrameLabelingEvaluator(ClamsAAPBEvaluationTask):
                 in_unit = ann.get_property('timeUnit')
                 s = data.get_start(ann)
                 e = data.get_end(ann)
-                pred_annotation[Segment(*(tuh.convert(t, in_unit, 'sec', fps) for t in (s, e)))] = ann.get_property('frameType')
+                label = ann.get_property(property_name)
+                #If a label is not predicted as en or es, it is relabeled as 'lr'
+                if self.task == 'lid' and (label != 'en' or label != 'es'):
+                    label = 'lr'
+                pred_annotation[Segment(*(tuh.convert(t, in_unit, 'sec', fps) for t in (s, e)))] = label
 
         return pred_annotation, gold
-
+    
     def _compare_pair(self, guid: str, gold: Any, pred: Any) -> Any:
         """
-        If evaluating CSD, detection error rate, precision, recall, and f1 are returned; 
-        however, pyannote's implementation of precision and recall is incorrect where if 
-        one timeline is empty but the other is not, one of the two metrics will be 1.0 when
-        both should be 0.0. 
+        If evaluating CSD, detection error rate, precision, recall, and f1 are returned;
+        however, pyannote's implementation of precision and recall is incorrect where if
+        one timeline is empty but the other is not, one of the two will be 1.0 when
+        both should be 0.0.
 
-        If evaluating LID, identification error rate and precision, recall, and f1 are returned;
+        If evaluating LID, identification error rate, precision, recall, and f1 are returned;
         however, because pyannote does not have a method that calculates f1, it is calculated here.
         """
-        err_rate = DetectionErrorRate()
-        prf1 = DetectionPrecisionRecallFMeasure()
+        if self.task == 'lid':
+            err_rate = IdentificationErrorRate()
+            p = IdentificationPrecision()(gold, pred)
+            r = IdentificationRecall()(gold, pred)
 
-        results_dict = prf1.compute_components(gold, pred)
-        p, r, f1 = prf1.compute_metrics(results_dict)
+            if p+r == 0:
+                f1 = 0.0
+            else:
+                f1 = (2*p*r)/(p+r)
+        else:
+            err_rate = DetectionErrorRate()
+            prf1 = DetectionPrecisionRecallFMeasure()
+            results_dict = prf1.compute_components(gold, pred)
+            p, r, f1 = prf1.compute_metrics(results_dict)
 
-        #Correcting precision and recall to 0.0 when one timeline is empty but the other is not
-        if bool(gold.get_timeline().duration()) != bool(pred.get_timeline().duration()): 
-            p=r=0.0
+            #Correcting precision and recall to 0.0 when one timeline is empty but the other is not
+            if bool(gold.get_timeline().duration()) != bool(pred.get_timeline().duration()):
+                p, r = (0.0, 0.0)
 
         return err_rate(gold, pred), p, r, f1
 
@@ -102,6 +129,7 @@ class TimeFrameLabelingEvaluator(ClamsAAPBEvaluationTask):
         call ``calculate_metrics`` with ``by_guid=False``.
         """
         raise NotImplementedError("Comparing all golds and preds is not implemented. ")
+
 
     def write_side_by_side_view(self):
         """
@@ -124,19 +152,20 @@ class TimeFrameLabelingEvaluator(ClamsAAPBEvaluationTask):
 
 if __name__ == "__main__":
     parser = TimeFrameLabelingEvaluator.prep_argparser()
-    parser.add_argument('--slate', action='store_true', help='slate annotations')
+    parser.add_argument('-t', '--task', choices=['chyron', 'slate', 'lid'], default='chyron')
 
     args = parser.parse_args()
     ref_dir = None
     if args.golds:
         ref_dir = args.golds
     else:
-        if args.slate:
+        if args.task == 'slate':
             ref_dir = goldretriever.download_golds(GOLD_SLATES_URL)
-        else:
+        elif args.task == 'chyron':
             ref_dir = goldretriever.download_golds(GOLD_CHYRON_URL)
 
-    evaluator = TimeFrameLabelingEvaluator(batchname=args.batchname, gold_loc=ref_dir, pred_loc=args.preds, slates=args.slate)
+    evaluator = TimeFrameLabelingEvaluator(batchname=args.batchname, gold_loc=ref_dir,
+                                           pred_loc=args.preds, task=args.task)
     evaluator.calculate_metrics(by_guid=True)
     report = evaluator.write_report()
     args.export.write(report.getvalue())
